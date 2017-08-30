a9f2f501510ddb56d4a0385effe9e9cf09593257
scottobot <Scott@sandoshs-imac.lan>
2017-08-29 21:15:00 -0700

    Initial Commit

---
+++ GiphyClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiphyClient
+//
+//  Created by Scott Doerrfeld on 8/29/17.
+//  Copyright © 2017 Scott Doerrfeld. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
